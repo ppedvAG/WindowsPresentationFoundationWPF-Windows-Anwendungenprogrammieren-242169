3eb3a4626ef33e456085918852d15ec4f8075878
ppedvLukasK <lukask@ppedv.de>
2024-10-15 11:21:57 +0200

    M005

---
+++ M005/MainWindow.xaml.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using System.Windows;
+
+namespace M005;
+
+public partial class MainWindow : Window, INotifyPropertyChanged
+{
+	private int zaehler;
+
+	/// <summary>
+	/// Die Variable MUSS ein Property sein (get; set;)
+	/// </summary>
+	public int Zaehler
+	{
+		get => zaehler;
+		set
+		{
+			zaehler = value;
+			Notify(); //nameof(Zaehler) -> "Zaehler"
+		}
+	}
+
+	//////////////////////////////////////////////////////////////////////////////////////
+
+	public ObservableCollection<int> Zahlen { get; set; } = [1, 2, 3, 4, 5];
+
+	public MainWindow()
+	{
+		InitializeComponent();
+		this.DataContext = this; //Das Fenster sieht sich selbst
+	}
+
+	private void Button_Click(object sender, RoutedEventArgs e)
+	{
+		Zaehler++;
+		Zahlen[0]++;
+	}
+
+	private void Button_Click_1(object sender, RoutedEventArgs e)
+	{
+		Zahlen.Add(Zahlen.Last() + 1);
+	}
+
+	//////////////////////////////////////////////////////////////////////////////////////
+
+	public event PropertyChangedEventHandler? PropertyChanged;
+
+	//public void Notify(string propName) => PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propName));
+
+	public void Notify([CallerMemberName] string propName = "") => PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propName));
+}
